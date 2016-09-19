a7e6ed9ed7b8fc13c35fa0912bd5a3bdd598991f
Bryan_2 <Bryan_2@BValerioH>
2016-09-18 19:37:16 -0600

    Se creo la clase de pruebas

---
+++ src/java/cr/ac/una/prograiv/proyecto/dao/test2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cr.ac.una.prograiv.proyecto.dao;
+
+/**
+ *
+ * @author Bryan_2
+ */
+public class test2 {
+    
+}
