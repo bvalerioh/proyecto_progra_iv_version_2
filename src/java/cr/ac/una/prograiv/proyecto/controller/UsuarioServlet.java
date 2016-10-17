4e144b0369281ac4b6638877dbb9e0ab34c66172
bvale <bvale@NELITA-PC>
2016-10-16 22:05:17 -0600

    creado servlet del usuario, creado funcion especial en usuarioDAo y BL, se modificaron unas cositas en el script, y en los datos personas se cambio el comando del boton submit a que llamara a  la funcion, falta hacer que direccione.

--- src/java/cr/ac/una/prograiv/proyecto/controller/UsuarioServlet.java
+++ src/java/cr/ac/una/prograiv/proyecto/controller/UsuarioServlet.java
@@ -11,8 +11,11 @@ import cr.ac.una.prograiv.proyecto.bl.UsuarioBL;
 import cr.ac.una.prograiv.proyecto.domain.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -44,7 +47,8 @@ public class UsuarioServlet extends HttpServlet {
             String json;
             
             //Se crea el objeto Direccion
-            Usuario usu = new Usuario();            
+            Usuario usu = new Usuario(); 
+            Usuario usuAux = new Usuario();
             UsuarioBL usuBL = new UsuarioBL();
             List<Usuario> liusu;
             //Se hace una pausa para ver el modal
@@ -64,18 +68,88 @@ public class UsuarioServlet extends HttpServlet {
                     json = new Gson().toJson(usuBL.findAll(Usuario.class.getName()));
                     out.print(json);
                     break;
-                case "guardarUsuario":
-                    String nombre,usuario,apellidos,email, direccion,telTrabajo,telCelular,contrasena,observaciones;
+                    
+                case "guardarUsuario": case "modificarUsuario":
+                    String nombre, usuario, apellidos,email, direccion,telTrabajo,telCelular,contrasena;
                     int tipoUsuario, estado, idUsuario;
-                    Date feNacimiento;
+                    
+                    usuario = request.getParameter("usuario");
+                    contrasena = request.getParameter("contra");
+                    nombre = request.getParameter("nombre");
+                    apellidos = request.getParameter("apellidos");
+                    email = request.getParameter("email");
+                    direccion = request.getParameter("direccion");
+                    telTrabajo = request.getParameter("telTrabajo");
+                    telCelular = request.getParameter("telCelular");
+                    
+                    try{
+                        idUsuario = Integer.parseInt(request.getParameter("idUsuario"));
+                    }catch(Exception e){
+                        idUsuario = 0; 
+                   }
+                    
+                        
+                    tipoUsuario = Integer.parseInt(request.getParameter("tipoUsuario"));
+                    estado = 1;
+                    
+                    //Guardar Correctamente en la base de datos
+                    String fechatxt = request.getParameter("fechaNacimiento");
+                    DateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+                    Date date = format.parse(fechatxt);
+                                     
+                    if(accion.equals("guardarUsuario")){ //es insertar personas
+                        usu = new Usuario(0, usuario, nombre, apellidos, email, date, direccion, telTrabajo, telCelular, contrasena, "Nuevo usuario", 0, estado);
+                        // Validación de que la persona no existe.
+                        usuAux = usuBL.findByUser(usu.getUsuario());
+                        if(usuAux == null){
+                            //Se guarda el objeto
+                            usuBL.save(usu);
+                            //Se imprime la respuesta con el response
+                            out.print("C~El usuario fue ingresado correctamente");
+                        }else{
+                            out.print("E~Ese usuario ya existe, cambielo por favor...");
+                        }
+                    }else{//es modificar persona
+                        //Se guarda el objeto
+                        usu = usuBL.findById(idUsuario);
+                        
+                        usu.setNombre(nombre);
+                        usu.setApellidos(apellidos);
+                        usu.setDireccion(direccion);
+                        usu.setEmail(email);
+                        usu.setFeNacimiento(date);
+                        usu.setTelCelular(telCelular);
+                        usu.setTelTrabajo(telTrabajo);
+                        if(!request.getParameter(contrasena).isEmpty()){
+                            usu.setContrasena(contrasena);
+                        }
+                        usuBL.merge(usu);
+
+                        //Se imprime la respuesta con el response
+                        out.print("C~La persona fue modificada correctamente");
+                    }
                     break;
-                case "unirUsuario":
-                    break;
-                case "eliminarUsuario":
+                    
+                case "eliminarUsuario":                    
+                       // elimina un usuario por id
+                        usu = usuBL.findById(Integer.parseInt(request.getParameter("idPersona")));
+                        //Se elimina el objeto
+                        usuBL.delete(usu);
+                        //Se imprime la respuesta con el response
+                        out.print("El usuario fue eliminado correctamente");                 
                     break;                    
+                    
                 case "usuarioXid":
+                    //se consulta el usuario por ID
+                    usu = usuBL.findById(Integer.parseInt(request.getParameter("idPersona")));
+                    
+                    //se pasa la informacion del objeto a formato JSON
+                    json = new Gson().toJson(usu);
+                    out.print(json);
                     break;
+                    
                 case "obtenerMisFacturas":
+                    // falta implemenetar.
                     break;
                 default:
                     out.print("E~No se indico la acción que se desea realizare");
